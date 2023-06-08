b98c733e330233a2f263fd2793b55d8798ad0cb4
confetti39 <hy04730@naver.com>
2023-06-08 20:23:28 +0900

    feat: 페이지 라우팅

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,45 @@
 import React from "react";
+import {
+  RecoilRoot,
+  atom,
+  selector,
+  useRecoilState,
+  useRecoilValue,
+} from "recoil";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
+import Home from "./pages/Home";
+import NotFound from "./pages/NotFound";
+import Detail from "./pages/Detail";
+import Add from "./pages/Add";
+import Login from "./pages/Login";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home />,
+    errorElement: <NotFound />,
+  },
+  {
+    path: "login",
+    element: <Login />,
+  },
+  {
+    path: "todos/:todoId",
+    element: <Detail />,
+  },
+  {
+    path: "add",
+    element: <Add />,
+  },
+]);
 
 function App() {
-  return <>App</>;
+  return (
+    <RecoilRoot>
+      <RouterProvider router={router} />
+    </RecoilRoot>
+  );
 }
 
 export default App;
