a2c766601f575401a9c346d6d128b6af6b2053e7
confetti39 <hy04730@naver.com>
2023-06-19 14:58:20 +0900

    feat: URL을 통해 에셋 가져오기

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -4,9 +4,18 @@ import { useNavigate } from "react-router-dom";
 
 export default function Header() {
   const navigate = useNavigate();
+  const iconUrl = new URL("../assets/to-do.png", import.meta.url).href;
   return (
-    <header className={styles.header} onClick={() => navigate(`/`)}>
-      TODO LIST
+    <header className={styles.container}>
+      <img
+        src={iconUrl}
+        className={styles.logo}
+        alt="todo icon"
+        onClick={() => navigate(`/`)}
+      />
+      <header className={styles.header} onClick={() => navigate(`/`)}>
+        TODO LIST
+      </header>
     </header>
   );
 }
